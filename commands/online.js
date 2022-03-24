0e883e62df7178b29e4b3b6e0ef414ec4a7e1f06
TheEmperorPhoenix <thephoenixempirehq@gmail.com>
2022-03-24 15:49:33 +0300

    automated beautification lmao

--- commands/online.js
+++ commands/online.js
@@ -10,93 +10,107 @@ module.exports = {
 		.setDescription('Показывает онлайн Тесея.'),
 	async execute(interaction) {
 
-        const res = await fetch(`https://mcapi.us/server/query?ip=theseus.su`) // Fetches current server status via mcapi
-        .catch(err => {
-            console.log("Failed to retrieve API response.");
-            console.error(err);
-            return;
-        });
+		const res = await fetch(`https://mcapi.us/server/query?ip=theseus.su`) // Fetches current server status via mcapi
+			.catch(err => {
+				console.log("Failed to retrieve API response.");
+				console.error(err);
+				return;
+			});
 
-        if (!res) {
-            return false;
-        } else {
-            const body = await res.json()
-            const status = (body.online ? "Online" : "Offline")
-            let message = new MessageEmbed()
-                .setTitle("Theseus")
-                .setColor("RED")
-                .setThumbnail("https://theseus.su/logo.png")
-                .setFooter(`Имейте в виду, что данные взяты с помощью стороннего API и могут быть устаревшими на несколько минут.`);;
+		if (!res) {
+			return false;
+		} else {
+			const body = await res.json()
+			const status = (body.online ? "Online" : "Offline")
+			let message = new MessageEmbed()
+				.setTitle("Theseus")
+				.setColor("RED")
+				.setThumbnail("https://theseus.su/logo.png")
+				.setFooter(`Имейте в виду, что данные взяты с помощью стороннего API и могут быть устаревшими на несколько минут.`);;
 
-            if (status == "Online") {
-                message.addField("Статус сервера", "Онлайн");
-                message.setColor("GREEN");
-                const players = body.players.now
-                const playersMax = body.players.max
+			if (status == "Online") {
+				message.addField("Статус сервера", "Онлайн");
+				message.setColor("GREEN");
+				const players = body.players.now
+				const playersMax = body.players.max
 
-                let sortedList = body.players.list.sort((a, b) => a.localeCompare(b)); // Sorting the list alphabetically
+				let sortedList = body.players.list.sort((a, b) => a.localeCompare(b)); // Sorting the list alphabetically
 
-                let playerList = "";
-                if (players > 0) { // Formatting the playerlist
-                    let i = 1;
-                    sortedList.forEach(item => { 
-                        playerList = playerList + i + ". " + item + ",\n"; 
-                        if (i == sortedList.length) playerList = playerList.slice(0, playerList.length - 2) + ".";
-                        i++;
-                 })
-                message.addField(`Текущий онлайн (${players}/${playersMax}):`, `${playerList}`); 
-                } else message.addField(`Текущий онлайн (0/${playersMax}):`, 'Ноль. Полотно пустует.');
-                
-                fs.readFile('persistent.json', 'utf-8', async (err, data) => {
-                    if (err)
-                        console.error(err);
-                    else {
-                        let persistent = JSON.parse(data);
-                        message.addField("Текущий рекорд онлайна: ", `${persistent.currentRecord.toString()} человек.`, true);
+				let playerList = "";
+				if (players > 0) { // Formatting the playerlist
+					let i = 1;
+					sortedList.forEach(item => {
+						playerList = playerList + i + ". " + item + ",\n";
+						if (i == sortedList.length) playerList = playerList.slice(0, playerList.length - 2) + ".";
+						i++;
+					})
+					message.addField(`Текущий онлайн (${players}/${playersMax}):`, `${playerList}`);
+				} else message.addField(`Текущий онлайн (0/${playersMax}):`, 'Ноль. Полотно пустует.');
 
-                        if (players > persistent.currentRecord) { // Announce the new record
-                            let announcementChannel = interaction.client.guilds.cache.get(guildID).channels.cache.get(channelID);
+				fs.readFile('persistent.json', 'utf-8', async (err, data) => {
+					if (err)
+						console.error(err);
+					else {
+						let persistent = JSON.parse(data);
+						message.addField("Текущий рекорд онлайна: ", `${persistent.currentRecord.toString()} человек.`, true);
 
-                            let embedAnnouncement = new MessageEmbed()
-                                .setTitle("Theseus")
-                                .setColor("AQUA")
-                                .setThumbnail("https://theseus.su/logo.png")
-                                .addField("Достигнут новый рекорд онлайна!", `Предыдущий рекорд: ${persistent.currentRecord}\nНовый рекорд: ${players}`)
-                                .addField("Онлайн в момент рекорда:", `${playerList}`)
-                                .setTimestamp();
+						if (players > persistent.currentRecord) { // Announce the new record
+							let announcementChannel = interaction.client.guilds.cache.get(guildID)
+								.channels.cache.get(channelID);
 
-                            if (persistent.recordMessage != 0)
-                                await announcementChannel.messages.delete(persistent.recordMessage)
-                                    .catch(console.error);
+							let embedAnnouncement = new MessageEmbed()
+								.setTitle("Theseus")
+								.setColor("AQUA")
+								.setThumbnail("https://theseus.su/logo.png")
+								.addField("Достигнут новый рекорд онлайна!", `Предыдущий рекорд: ${persistent.currentRecord}\nНовый рекорд: ${players}`)
+								.addField("Онлайн в момент рекорда:", `${playerList}`)
+								.setTimestamp();
 
-                            await announcementChannel.send({ embeds: [embedAnnouncement] })
-                                .then(message => persistent.recordMessage = message.id);
+							if (persistent.recordMessage != 0)
+								await announcementChannel.messages.delete(persistent.recordMessage)
+								.catch(console.error);
 
-                            persistent.currentRecord = players;
+							await announcementChannel.send({
+									embeds: [embedAnnouncement]
+								})
+								.then(message => persistent.recordMessage = message.id);
 
-                            fs.writeFile('persistent.json', JSON.stringify(persistent), 'utf-8', (err) => {
-                                if (err)
-                                    throw err;
-                            });
-                        }
-                    await interaction.reply({ embeds: [message], ephemeral: true })
-                    .catch(console.error); // Ephemeral reply to the interaction with the formatted message after everything's complete
-                    }
-                }); 
+							persistent.currentRecord = players;
 
-                interaction.client.user.setPresence({ // Update the presence
-					activities: [{ name: ("Тесей (" + players + "/" + playersMax + ")") }],
+							fs.writeFile('persistent.json', JSON.stringify(persistent), 'utf-8', (err) => {
+								if (err)
+									throw err;
+							});
+						}
+						await interaction.reply({
+								embeds: [message],
+								ephemeral: true
+							})
+							.catch(console.error); // Ephemeral reply to the interaction with the formatted message after everything's complete
+					}
+				});
+
+				interaction.client.user.setPresence({ // Update the presence
+					activities: [{
+						name: ("Тесей (" + players + "/" + playersMax + ")")
+					}],
 					status: 'online'
 				});
 
-            } else {
-                message.addField("Статус сервера", "Оффлайн");
-                interaction.reply({ embeds: [message], ephemeral: true });
-                interaction.client.user.setPresence({ 
-                    activities: [{ name: ("Тесей (оффлайн)"), type: "WATCHING" }],
-                    status: 'dnd'
-                });
-            };
-        };
-        },
+			} else {
+				message.addField("Статус сервера", "Оффлайн");
+				interaction.reply({
+					embeds: [message],
+					ephemeral: true
+				});
+				interaction.client.user.setPresence({
+					activities: [{
+						name: ("Тесей (оффлайн)"),
+						type: "WATCHING"
+					}],
+					status: 'dnd'
+				});
+			};
+		};
+	},
 };
